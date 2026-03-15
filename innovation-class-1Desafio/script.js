26a12bbeda270483dd93a24163efdf238bb43afa
guraiken <gustavo.fiabani@gmail.com>
2026-03-15 10:56:46 -0300

    fix: better responsiveness and fidelity

--- innovation-class-1Desafio/script.js
+++ innovation-class-1Desafio/script.js
@@ -12,12 +12,16 @@ const produtos = [
 
 const container = document.getElementById("card-container");
 const container2 = document.getElementById("card-container-2");
+const searchBar = document.getElementById("search-bar")
+
+searchBar.addEventListener('click', )
+
 
 produtos.forEach(produto => {
     container.innerHTML += `
     <div class="swiper-slide h-auto">
-        <div class="flex flex-col p-[8px] border border-gray-200 rounded-[8px] hover:shadow-lg transition-shadow bg-white relative group">
-            <span class="absolute top-[8px] left-[8px] bg-[#00264E] text-[#FFFF] text-[10px] text-center font-bold px-[6px] py-[2px] rounded-[4px] z-10 ">
+        <div class="flex flex-col h-full p-[8px] border border-[#dddddd] rounded-[8px] hover:shadow-lg transition-shadow bg-white relative group">
+            <span class="absolute top-[8px] left-[8px] bg-[#00264E] text-[#FFFF] text-[10px] text-center font-bold px-[6px] py-[2px] rounded-[4px] z-10">
                 ${produto.tag}
             </span>
 
@@ -28,30 +32,34 @@ produtos.forEach(produto => {
             <h3 class="text-[12px] text-[#303030] leading-snug mb-[10px] line-clamp-2">
                 ${produto.nome}
             </h3>
+            <div class="mb-[15px]">
+                <div class="flex items-center gap-[6px]">
+                    <div class="flex flex-col">
+                        <span class="text-[11px] text-[#303030] line-through leading-none">
+                            R$${produto.precoAntes}
+                        </span>
 
-            <div class="flex flex-col mt-auto mb-[15px]">
-                <span class="text-[11px] text-[#303030] line-through">R$${produto.precoAntes}</span>
+                        <span class="text-[16px] font-bold text-black leading-none mt-[4px]">
+                            R$${produto.precoDepois}
+                        </span>
+                    </div>
 
-                <div class="flex items-center gap-[5px]">
-                    <span class="text-[16px] font-bold text-black">R$${produto.precoDepois}</span>
-                    <span class="bg-[#5EC0BE] text-white text-[10px] font-bold px-[5px] py-[2px] rounded-[4px] underline">${produto.desconto}</span>
+                    <span class="bg-[#5EC0BE] text-white text-[11px] font-bold px-[6px] py-[4px] rounded-[4px] leading-none underline self-center">
+                        ${produto.desconto}
+                    </span>
                 </div>
-
-                <span class="text-[11px] text-[#303030] mt-[2px]">
+                <span class="block text-[11px] text-[#303030] mt-[6px]">
                     Ou em até <strong class="text-black font-semibold">${produto.parcela}</strong>
                 </span>
             </div>
-
-            <button class="w-full bg-[#005cff] hover:bg-[#004cd2] text-white font-bold text-[14px] py-[8px] rounded-[8px] font-[#FFFFFF] transition-colors cursor-pointer">
-                Comprar
-            </button>
+            <button class="w-full mt-auto bg-[#005cff] hover:bg-[#004cd2] text-white font-bold text-[14px] py-[8px] rounded-[8px] transition-colors cursor-pointer"> Comprar </button>
         </div>
     </div>
     `
     container2.innerHTML+= `
     <div class="swiper-slide h-auto">
-        <div class="flex flex-col p-[8px] border border-gray-200 rounded-[8px] hover:shadow-lg transition-shadow bg-white relative group">
-            <span class="absolute top-[8px] left-[8px] bg-[#00264E] text-[#FFFF] text-[10px] text-center font-bold px-[6px] py-[2px] rounded-[4px] z-10 ">
+        <div class="flex flex-col h-full p-[8px] border border-[#dddddd] rounded-[8px] hover:shadow-lg transition-shadow bg-white relative group">
+            <span class="absolute top-[8px] left-[8px] bg-[#00264E] text-[#FFFF] text-[10px] text-center font-bold px-[6px] py-[2px] rounded-[4px] z-10">
                 ${produto.tag}
             </span>
 
@@ -62,23 +70,27 @@ produtos.forEach(produto => {
             <h3 class="text-[12px] text-[#303030] leading-snug mb-[10px] line-clamp-2">
                 ${produto.nome}
             </h3>
+            <div class="mb-[15px]">
+                <div class="flex items-center gap-[6px]">
+                    <div class="flex flex-col">
+                        <span class="text-[11px] text-[#303030] line-through leading-none">
+                            R$${produto.precoAntes}
+                        </span>
 
-            <div class="flex flex-col mt-auto mb-[15px]">
-                <span class="text-[11px] text-[#303030] line-through">R$${produto.precoAntes}</span>
+                        <span class="text-[16px] font-bold text-black leading-none mt-[4px]">
+                            R$${produto.precoDepois}
+                        </span>
+                    </div>
 
-                <div class="flex items-center gap-[5px]">
-                    <span class="text-[16px] font-bold text-black">R$${produto.precoDepois}</span>
-                    <span class="bg-[#5EC0BE] text-white text-[10px] font-bold px-[5px] py-[2px] rounded-[4px] underline">${produto.desconto}</span>
+                    <span class="bg-[#5EC0BE] text-white text-[11px] font-bold px-[6px] py-[4px] rounded-[4px] leading-none underline self-center">
+                        ${produto.desconto}
+                    </span>
                 </div>
-
-                <span class="text-[11px] text-[#303030] mt-[2px]">
+                <span class="block text-[11px] text-[#303030] mt-[6px]">
                     Ou em até <strong class="text-black font-semibold">${produto.parcela}</strong>
                 </span>
             </div>
-
-            <button class="w-full bg-[#005cff] hover:bg-[#004cd2] text-white font-bold text-[14px] py-[8px] rounded-[8px] font-[#FFFFFF] transition-colors cursor-pointer">
-                Comprar
-            </button>
+            <button class="w-full mt-auto bg-[#005cff] hover:bg-[#004cd2] text-white font-bold text-[14px] py-[8px] rounded-[8px] transition-colors cursor-pointer"> Comprar </button>
         </div>
     </div>
     `
